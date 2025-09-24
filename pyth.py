ac21f54cf0025fd10ee59ad345663ea672a97337
Jerald1904 <156393612+Jerald1904@users.noreply.github.com>
2025-09-24 19:49:36 +0530

    Update pyth.py

--- pyth.py
+++ pyth.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello World, This is Albert!"
+    return "Hi, I'm ALBERT JERALD M from Chennai, Tamil Nadu. Email ID: albertjerald19@gmail.com Ph.no: 8072545136"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
